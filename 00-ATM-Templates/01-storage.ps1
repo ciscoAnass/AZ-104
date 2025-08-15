6d4dc4f3859eba79e0d33f771f70f3e079fca88b
An1x24 <anass.assim.2010@gmail.com>
2025-08-15 04:42:56 +0200

    Delete storage files

--- 00-ATM-Templates/01-storage.ps1
+++
@@ -1,7 +0,0 @@
-$rg = 'arm-introduction-01'
-New-AzResourceGroup -Name $rg -Force
-
-New-AzResourceGroupDeployment `
-    -Name 'new-storage' `
-    -ResourceGroupName $rg `
-    -TemplateFile '01-storage.json' 
